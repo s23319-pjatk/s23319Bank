2a26589d750eef940d6732f6cb6ba24f4f27b463
s23319-pjatk <73029837+s23319-pjatk@users.noreply.github.com>
2025-01-25 15:43:52 +0100

    Add files via upload
    test

---
+++ src/main/java/pl/pjatk/s23319Bank/S23319BankApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s23319Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S23319BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S23319BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/s23319Bank/S23319BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s23319Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S23319BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
